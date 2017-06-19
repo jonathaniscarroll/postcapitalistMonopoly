19660f0d20f232dc0126062927dae183184026c4
Jonathan Carroll <jonathan@toughguymountain.com>
2017-06-19 15:43:25 +0200

    choose between simulation and player powered, create automagic players

--- PostCapitalistPropaganda/Assets/simulateMonopoly.cs
+++ PostCapitalistPropaganda/Assets/simulateMonopoly.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class simulateMonopoly : MonoBehaviour {
 
+	public GameObject playerPanel;
+	public GameObject simulationChoicePanel;
+
+	private Lexic.NameGenerator namegen;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		namegen = GetComponent<Lexic.NameGenerator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void activateSimulation(bool activate){
+		playerPanel.SetActive (false);
+		Debug.Log (activate);
+		if (activate) {
+			addPlayers addplayers = GetComponent<addPlayers> ();
+			for (int i = 0; i < Random.Range (2, 10); i++) {
+				string name = namegen.GetNextRandomName ();
+				addplayers.populateList (name);
+				Debug.Log("adding player " + name);
+			}
+			addplayers.createPlayers ();
+
+		} else {
+			simulationChoicePanel.SetActive (false);
+		}
+	}
+
+	void aiTurn(){
+
 	}
 }
